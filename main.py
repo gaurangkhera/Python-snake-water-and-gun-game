4d4a2ec87e4595118e48fb07e3a37d80309e3d42
Gaurang Khera <khera.gaurang@gmail.com>
2022-02-19 17:01:50 +0530

    Upload required files
    A snake, water and gun game made in python

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+def snake_water_gun(computer, player):
+    if computer == 's' and player == 'g':
+        return True
+    elif computer == 'w' and player == 's':
+        return True
+    elif computer == 'g' and player == 'w':
+        return True
+    else:
+        return False
+
+choice = ('s', 'w', 'g')
+computer = random.randint(0,2)
+computer = choice[computer]
+player = input("Choose either snake(s), water(w) or gun(g): ").lower()
+
+win = snake_water_gun(computer, player)
+
+if win==True:
+    print("The computer chose " + computer + " and you chose " + player + ". You win!")
+else:
+    print("The computer chose " + computer + " and you chose " + player + ". You lose!")
